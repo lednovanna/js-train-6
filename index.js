9b41b268e59a6b7149e0277c81ce9740ebf0162c
lednovanna <lelagency@gmail.com>
2024-04-04 10:06:19 +0100

    train6

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Завдання 1.Cтворіть об'єкт person за допомогою конструктора з полями name: "John",age: 25
-let person = {};
+let person = {name: "John", age: 25};
 
 console.log("Завдання 1 ====================================");
 
@@ -10,7 +10,13 @@ console.log("person", person); // Виведе {name: "John", age: 25}
 let personLarge = {
   //використовуємо деструктурізацію на об'єкті person
   //створюємо об'єкт address
+  name: "John", 
+  age: 25,
+  adress: {street: "123 Main  ST", city: "New York"}
+  
 };
+
+
 
 console.log("Завдання 2 ====================================");
 console.log("personLarge", personLarge); // Виведе
@@ -31,7 +37,10 @@ var animal = {
 function copyObject(obj) {
   // Використовуємо синтаксис деструктурізації {...person} для створення нового об'єкта з тими ж властивостями
   // Повертаємо новий об'єкт
+    return {...obj};
+    
 }
+var copiedAnimal = copyObject(animal);
 
 console.log("Завдання 3 ====================================");
 
@@ -47,6 +56,11 @@ function hasProperty(obj, property) {
   // Використовуємо оператор "in" для перевірки наявності властивості
   // Запишимо умову якщо властивість існує повертає текст Property ${property} exists,
   // інашке повертаємо Property ${property} does not exist.
+  if (property in obj) {
+    return `Property ${property} exists`;
+  } else {
+    return `Property ${property} does not exist`;
+  }
 }
 
 console.log("Завдання 4 ====================================");
@@ -62,6 +76,9 @@ let country = {
 function printKeysAndValues(obj) {
   // Проходимося по всіх ключах об'єкту за допомогою циклу "for in"
   // Виводимо ключ та значення на консоль
+  for (let key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+  }
 }
 
 console.log("Завдання 5 ====================================");
@@ -79,6 +96,8 @@ let movie = {
 function deleteProperty(obj, property) {
   // Використовуємо оператор "delete" для видалення властивості
   // Повертаємо об'єкт
+  delete obj[property];
+  return obj;
 }
 
 console.log("Завдання 6 ====================================");
@@ -91,6 +110,10 @@ let user = {
   age: 25,
   // Створюємо метод introduce, який за допомогою ключового слова this має повернути такий рядок
   // My name is John and I am 25 years old.
+
+  introduce: function() {
+    return `My name is ${this.name} and I am ${this.age} years old.`;
+  }
 };
 
 console.log("Завдання 7 ====================================");
@@ -109,6 +132,8 @@ let book = {
 function addField(obj, newField, value) {
   // Додаємо нове поле до об'єкту з допомогою квадратних дужок
   // Повертаємо об'єкт
+    obj[newField] = value;
+    return obj;
 }
 
 console.log("Завдання 8 ====================================");
@@ -123,6 +148,9 @@ let laptop = {
 function destructureObject(obj) {
   // Використовуємо деструктуризацію для створення нових змінних з властивостей об'єкту і отримуємо з нього змінні brand та model
   // Повертаємо нові змінні  в форматі 'Brand: ${brand}, Model: ${model}'
+  const { brand, model } = obj;
+  
+  return `Brand: ${brand}, Model: ${model}`;
 }
 
 console.log("Завдання 9 ====================================");
@@ -141,6 +169,12 @@ function changeRole(array, newRole) {
   // Ітеруємося по масиву об'єктів за допомогою циклу "for of"
   // Змінюємо роль кожного користувача на нове ім'я
   // Виводимо об'єкт на консоль
+  for (let user of array) {
+  
+    user.role = newRole;
+
+    console.log(user);
+  }
 }
 
 console.log("Завдання 10 ====================================");
@@ -163,7 +197,12 @@ let product = {
 function printProductDetails(obj) {
   // Використовуємо деструктуризацію для отримання значень productName, price i також значень companyName, country вкладеного об'єкту manufacturer
   // Виводимо productName, price, companyName та country на консоль
+  let { productName, price, manufacturer: { companyName, country } } = obj;
+  console.log(`Product Name: ${productName}`);
+  console.log(`Price: $${price}`);
+  console.log(`Manufacturer: ${companyName} (${country})`);
 }
+
 
 console.log("Завдання 11 ====================================");
 printProductDetails(product); // Виведе Smartphone 699 Tech Corp USA
@@ -179,6 +218,11 @@ function compareObjects(obj1, obj2) {
   // Виводимо результат порівняння об'єктів
   // Присвоємо obj2 значення об'єкту obj1
   // Виводимо результат порівняння об'єктів
+  console.log(obj1 === obj2);
+  obj2 = obj1;
+
+  // Перевіряємо, чи об'єкти рівні після присвоєння
+  console.log(obj1 === obj2);
 }
 
 console.log("Завдання 12 ====================================");
@@ -200,6 +244,7 @@ function showCarInfo({
   country = "Unknown",
 } = {}) {
   // Повертаємо об'єкт зі значеннями властивостей
+  return { brand, year, country };
 }
 
 console.log("Завдання 13 ====================================");
@@ -210,6 +255,7 @@ console.log(showCarInfo(car)); // Виведе { brand: 'BMW', year: 2022, count
 function addProperty(array) {
   // Додаємо нову властивість customProperty до прототипу Array зі значенням myProperty
   // Повертаємо переданий масив з новою властивістю
+ 
 }
 
 console.log("Завдання 14 ====================================");
